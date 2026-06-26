0d8d2846377da61fa838fc5fc003b63c382298d9
nilsb1 <40722762+nilsb1@users.noreply.github.com>
2026-06-26 06:12:09 +0200

    lower maxdur

--- apps/web/app/(ee)/api/cron/payouts/charge-succeeded/route.ts
+++ apps/web/app/(ee)/api/cron/payouts/charge-succeeded/route.ts
@@ -12,7 +12,7 @@ import { sendPaypalPayouts } from "./send-paypal-payouts";
 import { getFundSettlementTiming, scheduleDelayedPayouts } from "./utils";
 
 export const dynamic = "force-dynamic";
-export const maxDuration = 600; // This function can run for a maximum of 10 minutes
+export const maxDuration = 300; // This function can run for a maximum of 10 minutes
 
 const payloadSchema = z.object({
   invoiceId: z.string(),
